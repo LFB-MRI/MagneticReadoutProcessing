6a8c8f9f9bdcce2eaf0f78734577af30d9b181d1
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-07-12 22:24:00 +0200

    Create test_MRPAnalysis.py

---
+++ test/test_MRPAnalysis.py
@@ -0,0 +1,50 @@
+import math
+
+import numpy as np
+import pytest
+import unittest
+import random
+from MagneticReadoutProcessing import MRPConfig
+from MagneticReadoutProcessing import MRPReading
+import configparser
+import os
+class TestMPRAnalysis(unittest.TestCase):
+
+    # PREPARE A INITIAL CONFIGURATION FILE
+    def setUp(self) -> None:
+        # USE DEFAULT CONFIG
+        self.config = MRPConfig.MRPConfig(None)
+        self.config.load_defaults()
+
+        self.reading_A = MRPReading.MRPReading(self.config)
+        self.reading_B = MRPReading.MRPReading(self.config)
+        self.assertIsNotNone(self.reading_A)
+        self.assertIsNotNone(self.reading_B)
+
+        self.reading_A.sensor_id = 0
+        self.reading_B.sensor_id = 0
+
+        n_phi = self.config.MEASUREMENT_HORIZONTAL_RESOLUTION
+        n_theta = self.config.MEASUREMENT_VERTICAL_RESOLUTION
+        # CREATE A POLAR COORDINATE GRID TO ITERATE OVER
+        theta, phi = np.mgrid[0.0:0.5 * np.pi:n_theta * 1j, 0.0:2.0 * np.pi:n_phi * 1j]
+
+        ii = 0
+        jj = 0
+        for j in phi[0, :]:
+            ii = ii + 1
+            for i in theta[:, 0]:
+                jj = jj + 1
+                self.reading_A.insert_reading(random.uniform(0, 1)*10.0, j, i, ii, jj, random.uniform(0, 1) * 10.0 + 25.0)
+                self.reading_B.insert_reading(random.uniform(0, 1)*10.0, j, i, ii, jj, random.uniform(0, 1) * 10.0 + 25.0)
+    # JUST USED FOR PREPERATION
+
+
+    def test_calibration_analysis(self):
+        pass
+
+    def test_merge_analysis(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
