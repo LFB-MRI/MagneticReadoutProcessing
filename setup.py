f83592476319e2eefe7825c9c58adc16f8e56fef
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-07-12 17:34:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='MagneticReadoutProcessing',
       version='1.0',
-      description='Python Distribution Utilities',
+      description='Process raw data from magnetometers',
       author='Marcel Ochsendorf',
       author_email='info@marcelochsendorf.com',
       url='https://github.com/LFB-MRI/MagnetCharacterization/',
