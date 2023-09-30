eb0e9dc205ef0641fb0b12d689df413f32bea5d3
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-09-30 18:05:52 +0200

    Update udpp_editor.py

--- src/MagneticReadoutProcessing/udpp/udpp_editor.py
+++ src/MagneticReadoutProcessing/udpp/udpp_editor.py
@@ -1,6 +1,7 @@
 """Typer base cli interface to allow the user to interact with the udppf system"""
 
 import os
+import signal
 from pathlib import Path
 from typing import Annotated
 import typer
@@ -18,6 +19,15 @@ flask_server: multiprocessing.Process = None
 PIPELINES_FOLDER = str(Path(str(os.path.dirname(__file__))).parent.joinpath("pipelines"))
 TMP_FOLDER = str(Path(PIPELINES_FOLDER).joinpath("generated/"))
 
+terminate_flask: bool = False
+
+def signal_andler(signum, frame):
+    global terminate_flask
+    terminate_flask = True
+    time.sleep(4)
+    exit(1)
+signal.signal(signal.SIGINT, signal_andler)
+
 
 @app_flask.route("/")
 def index():
@@ -25,21 +35,34 @@ def index():
 
 
 
-def flask_server_task(Conf):
-   app_flask.run(host='0.0.0.0', port=1337,)
+def flask_server_task(_config: dict):
+    host:str = _config.get("host", "0.0.0.0")
+    port: int = _config.get("port", 5000)
+    debug: bool = _config.get("debug", False)
+
+    # TODO SET STATIC FOLDER
+    #
+    app_flask.run(host=host, port=port, debug=debug)
+
 
 
 
 @app_typer.command()
-def launch(ctx: typer.Context, port: int = 5000, host: str = "0.0.0.0"):
+def launch(ctx: typer.Context, port: int = 5555, host: str = "0.0.0.0", flask_debug: bool = True):
+    global terminate_flask
     #flask_server.start()
-    flask_config = {"port": port, "host": host}
-    flask_server = multiprocessing.Process(target=flask_server_task, args=(flask_config,))
+    flask_config = {"port": port, "host": host, "debug":flask_debug}
+    flask_server:multiprocessing.Process = multiprocessing.Process(target=flask_server_task, args=(flask_config,))
+    flask_server.start()
+
+    while( not terminate_flask):
+        if typer.prompt("Terminate  [Y/n]", 'y') == 'y':
+            break
 
 
-    while(True):
-        time.sleep(1)
-
+    # STOP
+    flask_server.terminate()
+    flask_server.join()
 
 
 
