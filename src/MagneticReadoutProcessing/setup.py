5a4fab97bd761ca2d21bb927d07c28952886ca9c
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2023-11-01 13:29:58 +0100

    .

--- src/MagneticReadoutProcessing/setup.py
+++ src/MagneticReadoutProcessing/setup.py
@@ -3,20 +3,19 @@
 from distutils.core import setup
 import pathlib
 import pkg_resources
-import setuptools
-import os
+
 
 req_path = 'requirements.txt' # os.path.join(os.path.dirname(__file__), 'requirements.txt')
 with pathlib.Path(req_path).open() as requirements_txt:
     install_requires = [str(requirement) for requirement in pkg_resources.parse_requirements(requirements_txt)]
 
 setup(name='MagneticReadoutProcessing',
-      version='1.3.1',
-      description='Process raw data from magnetometers',
+      version='1.3.2',
+      description='This library was created for the Low-Field MRI project and allows processing of data measured by magnetic field sensors. The focus is on visualization, followed by the provision of simple interfaces to work with this data. In general its possible to use this lib on all kinds of sensor data.',
       author='Marcel Ochsendorf',
       author_email='info@marcelochsendorf.com',
       url='https://github.com/LFB-MRI/MagnetCharacterization/',
-      packages= ['MRP', 'MRPcli', 'MRPudpp', 'tests'],#setuptools.find_packages('src', exclude=['test'], include=['MRPcli']),
+      packages= ['MRP', 'MRPcli', 'MRPudpp'],
       install_requires=install_requires,
       include_package_data=True,
       package_data={"": ["*.mag.json", "*.yaml", "*.html", "*.js", "*.css", "*.md", "*.json", "*.ts", "*.xml"]},
